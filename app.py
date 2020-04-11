7d8731064d07f0baf167dd46928194ddb6f9ab0d
melis <makce2@gmail.com>
2020-04-11 13:04:57 -0500

    first class

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello good starting")
+class Student:
+    def __init__(self, new_name, new_grades):
+        self.name = new_name
+        self.grades = new_grades
+
+    def average(self):
+        return sum(self.grades) / len(self.grades)
+
+student_one = Student('Rolf', [45, 56, 67, 88])
+student_two = Student('Nekus', [56, 87, 99, 54])
+print(student_one.grades)
