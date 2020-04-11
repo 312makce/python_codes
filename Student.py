b30b39b823f145f0089387e31c9a47c0d611936b
melis <makce2@gmail.com>
2020-04-11 15:19:54 -0500

    class inheritence examples

--- Student.py
+++ Student.py
@@ -1,3 +1,24 @@
-def student():
-    raise ("i starting working on it")
-print("never mind")
+class Student:
+    def __init__(self, name, school):
+        self.name = name
+        self.school = school
+        self.mark = []
+
+    def average(self):
+        return sum(self.mark) / len(self.mark)
+
+class WorkingStudent(Student):
+    def __init__(self, name, school, salary):
+        super(WorkingStudent, self).__init__(name, school)
+        self.salary = salary
+    @property
+    def weekly_salary(self):
+        return self.salary * 40
+
+melis = WorkingStudent('Melis', 'IAAU', 55)
+print(melis.salary)
+melis.mark.append(77)
+melis.mark.append(87)
+print(f'average of your marks {melis.average()} ')
+print(melis.weekly_salary)
+
